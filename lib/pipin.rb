f63190ebc8c36f5ea1b327aa9da943c23091d621
dan5 <dan5.ya@gmail.com>
2011-06-20 23:21:41 +0900

    new build コマンドの雛形作成

--- lib/pipin.rb
+++ lib/pipin.rb
@@ -1,6 +1,25 @@
 require "pipin/version"
 
 module Pipin
-  # Your code goes here...
-  puts 'Hello Pipin!'
+  def self.command(cmd)
+    __send__ "command_#{cmd}", *ARGV
+  end
+
+  def self.command_new(dir)
+    # todo: ./new_tamplate をコピーしたほうがいいか
+    #[
+    #  [:mkdir, dir],
+    #  [:create, dir + '/pipinrc'],
+    #]
+    puts "    create #{dir}"
+    Dir.mkdir dir
+    Dir.chdir(dir) {
+      puts "    create #{dir}/pipinrc"
+      File.open('pipinrc', 'w') {}
+    }
+  end
+
+  def self.command_build(dir = '.')
+    load dir + '/pipinrc'
+  end
 end
