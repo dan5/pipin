81eb4ed3c2a3990972c6cdd5d12519d35552bb23
dan5 <dan5.ya@gmail.com>
2011-06-22 08:54:28 +0900

    renderをBuildクラスに

--- lib/pipin.rb
+++ lib/pipin.rb
@@ -26,28 +26,40 @@ module Pipin
 
   def self.command_build(dir = '.')
     load dir + '/pipinrc'
-    Post.find('20*').each do |post|
-      write_html post.label, render_with_layout(:post, binding)
+    build = Builder.new('public')
+    build.render_posts
+    build.render_list('index', '20*', :limit => 3)
+  end
+
+  class Builder
+    def initialize(dstdir)
+      @dstdir = dstdir
     end
-    posts = Post.find('20*', :limit => 3)
-    write_html 'index', render_with_layout(:list, binding)
-  end
 
-  def self.write_html(label, html)
-    p label
-    dir = 'public'
-    Dir.chdir(dir) {
-      File.open("#{label}.html", 'w') {|f| f.write html }
-    }
-  end
+    def render_posts
+      Post.find('20*').each do |post|
+        write_html post.label, render_with_layout(:post, binding)
+      end
+    end
 
-  def self.render_with_layout(template, b = binding)
-    render(:layout) { render(template, b) }
-  end
+    def render_list(name, pattern, options = {})
+      posts = Post.find(pattern, options)
+      write_html name, render_with_layout(:list, binding)
+    end
 
-  def self.render(template, b = binding)
-    haml = File.read("../views/#{template}.haml")
-    Haml::Engine.new(haml).render(b)
+    def write_html(label, html)
+      p label
+      File.open(File.join(@dstdir, "#{label}.html"), 'w') {|f| f.write html }
+    end
+
+    def render_with_layout(template, b = binding)
+      render(:layout) { render(template, b) }
+    end
+
+    def render(template, b = binding)
+      haml = File.read("../views/#{template}.haml")
+      Haml::Engine.new(haml).render(b)
+    end
   end
 
   class Post
@@ -55,7 +67,7 @@ module Pipin
     def self.find(pattern, options = {})
       pattern.gsub!(/[^\d\w_*]/, '') # DON'T DELETE!!!
       files = Dir.chdir(@@entries_dir) { Dir.glob(pattern) }.sort.reverse
-      files = files.shift(options[:limit]) if options[:limit]
+      files = files[0, options[:limit]] if options[:limit]
       files.map {|e| Post.new(e) }
     end
 
